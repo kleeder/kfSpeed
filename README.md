cdd3e95a1589b7c10b3b1e54838c83b7b4d02739
kleeder <5kleeman@informatik.uni-hamburg.de>
2022-02-07 20:31:05 +0100

    command line arguments, autogenerating the whole .kftm if nothing else is specified, print-statements for progress on conversion, better error-handling(?)

--- README.md
+++ README.md
@@ -1,20 +1,35 @@
 # kfSpeed
 
-This is a very simple tool to convert raw pcm data into klangfreude.
+This is a very simple tool to convert raw pcm data into [klangfreude](https://kleeder.de/files/botb/klangfreude/klangfreude_20210508.zip).
 It works by DC-offsetting silence.
-Adjusting and setting up your .KFTM is needed for this to work.
+Adjusting and setting up your .KFTM is needed for this to work, unless you wanna generate one from scratch.
 Since klangfreude behaves differently depending on where you wanna start the PCM,
 brute-forcing the correct values is needed. I cannot help you with this.
+-------
+## Setup
+
 Make sure you have the following files in your directory:
-- music.wav (all kHz work, but keep the file size limit in mind)
-- kfSpeedRaw.KFTM (included with the tool; only works if you want PCM directly at the start)
+- mandatory
+  - music.wav (all kHz work, but keep the file size limit in mind)
+- optional
+  - kfSpeedRaw.KFTM 
 
-There are ideas for 2-channel-PCM, which is WIP and uncommented for now.
-Other improvements to be made:
-- generating the whole .KFTM aka not requiring a raw-file,
-  unless you specify it.
+Now just run the script by using
+  ```bash
+   python main.py music.wav
+  ```
+If you don't specify a .kftm, it will just generate a new output.KFTM.
+If you specify a .kftm by running the following:
+  ```bash
+   python main.py music.wav file.kftm
+  ```
+it will append to an already existing .kftm file.
+
+Possible improvements to be made:
 - specify wether you want 1-channel or 2-channel-PCM
-
+-------
 ## Version history
 
+* 0.2: more settings and auto-generating the whole kftm if nothing else is specified
 * 0.1: alpha-version. Partially working tool, yay!
+-------
--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
 
-"""
-kfSpeed, version 0.1
-----------------------
-
-This is a very simple tool to convert raw pcm data into klangfreude.
-It works by DC-offsetting silence.
-Adjusting and setting up your .KFTM is needed for this to work.
-Since klangfreude behaves differently depending on where you wanna start the PCM,
-brute-forcing the correct values is needed. I cannot help you with this.
-
-Make sure you have the following files in your directory:
-- music.wav (all kHz work, but keep the file size limit in mind)
-- kfSpeedRaw.KFTM (included with the tool; only works if you want PCM directly at the start)
-
-
-There are ideas for 2-channel-PCM, which is WIP and uncommented for now.
-Other improvements to be made:
-- generating the whole .KFTM aka not requiring a raw-file,
-  unless you specify it.
-- specify wether you want 1-channel or 2-channel-PCM
-
-Version history
----------------
-
-* 0.1: alpha-version. Partially working tool, yay!
-
-"""
+# kfSpeed Version 0.2
 
 import binascii
 import wave
 import struct
+from sys import argv, stderr
+import os.path
+
+
+def die(msg):
+    if isinstance(msg, BaseException):
+        msg = str(msg)
+    stderr.write(str(msg) + '\n')
+    exit(1)
+
+def open_wav(wav):
+    # read the pcm
+    try:
+        with wave.open(wav, 'rb') as wr:
+            return wr.readframes(wr.getnframes()), wr.getframerate()
+    except:
+        die(".wav file not found.")
+
+def open_kftm(kftm):
+    # read the pcm
+    try:
+        return open(kftm, "ab")
+    except:
+        die(".kftm file not found.")
+
+
+def get_usage(self_py):
+    return 'Usage: {} music.wav\n\nOptional parameters:\nexisting.KFTM (to add data to an existing .KFTM)\ntwochannel (to add a second channel of PCM data (not implemented))\n\nmain.py music.wav existing.KFTM twochannel'.format(self_py)
 
 
 # necessary funtion to convert the double values into hex format
@@ -38,9 +40,69 @@ def double_to_hex(f):
     return hex(struct.unpack('<Q', struct.pack('<d', f))[0])
 
 
-# read the pcm
-with wave.open('music.wav', 'rb') as wr:
-    frames = wr.readframes(wr.getnframes())
+def reverse_hex_array(hex_value):
+    hexArray = [hex_value[i:i + 2] for i in range(0, len(hex_value), 2)]
+    return hexArray[8] + hexArray[7] + hexArray[6] + hexArray[5] + hexArray[4] + hexArray[3] + hexArray[2] + hexArray[1]
+
+
+def generate_KFTM():
+    output_file = open("output.KFTM", "wb")
+    output_string = "6B66746D0100"  # KFTM header
+    output_string += "0"*128  # Title
+    output_string += "0"*128  # Composer
+    output_string += "0"*128  # Description
+    output_string += "FFFF"  # Total amount of rows
+    # Content of the first 4 rows (this is stupidly long and could be shortend, but I am lazy rn)
+    output_string += "01000000000000408F40000000000000F0BF000000000000F03F000000000000F0BF0000000000E07540000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000F0BF000000000000F03F000000000000F0BF000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+
+    output_file.write(binascii.unhexlify(output_string))
+    output_file.close()
+
+
+
+
+MUSIC = None
+KFRAW = None
+TWOCHANNEL = False
+
+if len(argv) == 1:
+    die(get_usage(argv[0]))
+
+for arg in argv:
+    if arg.endswith('.wav'):
+        if MUSIC is None:
+            MUSIC = arg
+        else:
+            die(get_usage(argv[0]))
+    if arg.lower().endswith('.kftm'):
+        if KFRAW is None:
+            KFRAW = arg
+        else:
+            die(get_usage(argv[0]))
+    if arg == 'twochannel':
+        die("Twochannel mode is not implemented yet.")
+
+
+if MUSIC is None:
+    die(get_usage(argv[0]))
+
+
+MUSIC, framerate = open_wav(MUSIC)
+print(".wav file opened. Frequency: {}Hz - Speed used for kf-emulation: s{}".format(str(framerate), str(framerate/8)))
+
+if KFRAW is None:
+    generate_KFTM()
+    KFRAW = open_kftm("output.KFTM")
+    print("generated a new kftm file \"output.kftm\"")
+else:
+    if os.path.isfile(KFRAW):
+        print("appending to {}".format(KFRAW))
+        KFRAW = open_kftm(KFRAW)
+    else:
+        die("{} is not an existing file.".format(KFRAW))
+
+
+
 
 # this part is only needed if you wanna write a 2nd part into your file on a different channel.
 # more setup is required for this
@@ -49,8 +111,8 @@ with wave.open('music.wav', 'rb') as wr:
 # with wave.open('music2.wav', 'rb') as wr:
 #     frames2 = wr.readframes(wr.getnframes())
 
-# setting the beginning and end of the rows, as hex (all 00)
-rowStart = "0" * 20 #20
+# setting the beginning of the first row (the speed is wav-hz/8) and the end of the row (bunch of empty columns)
+rowStart = "0100" + reverse_hex_array((double_to_hex((framerate/8))).ljust(18, "0"))
 rowEnd = "0" * 240
 
 # alternative version for two channels
@@ -58,20 +120,21 @@ rowEnd = "0" * 240
 # rowEnd = "0" * 48 #240
 # rowEnd2 = "0" * 176 #240
 
-# The raw KFTM
-kfraw = open("kfSpeedRaw.KFTM", "ab")
 
 # for debugging reasons, this is tracked.
 # You need to change the division later on according to the peaks here.
 lowest = 2000
 highest = 0
 
+
+print("writing pcm into .kftm")
+
 # main loop to write all info into the .KFTM file
-for offset in range(0, len(frames), 2):
+for offset in range(0, len(MUSIC), 2):
     # change the offset depending on how many frames you wanna skip in the song
     # this changes the quality (and saves file size)
     if offset%32 == 0:
-        data_new = (struct.unpack_from('<h', frames, offset)[0])
+        data_new = (struct.unpack_from('<h', MUSIC, offset)[0])
 
         # depending on where you start your PCM, these values need to be changed.
         # currently uncommented line is for the example raw KFTM.
@@ -80,8 +143,7 @@ for offset in range(0, len(frames), 2):
         # this one got used by me for my ac21 entry. can be ignored.
         #x = int(((data_new + 31540)/21)) #415
         doubleHex = (double_to_hex(x)).ljust(18, "0")
-        hexArray = [doubleHex[i:i + 2] for i in range(0, len(doubleHex), 2)]
-        rowMiddle = hexArray[8]+hexArray[7]+hexArray[6]+hexArray[5]+hexArray[4]+hexArray[3]+hexArray[2]+hexArray[1]
+        rowMiddle = reverse_hex_array(doubleHex)
 
         # this stuff is only needed for 2-channel PCM
         # try:
@@ -95,10 +157,14 @@ for offset in range(0, len(frames), 2):
         # row = rowStart + rowMiddle + rowEnd + rowMiddle2 + rowEnd2
 
         row = rowStart + rowMiddle + rowEnd
-        kfraw.write(binascii.unhexlify(row))
+        KFRAW.write(binascii.unhexlify(row))
         lowest = min(lowest, x)
         highest = max(highest, x)
+        # important to set this to 0 now, so the speed command is not written into every new row
+        # (even tho it wldnt hurt to do this, it looks cleaner if we skip it)
+        rowStart = "0" * 20  # 20
 
-kfraw.close()
-print(lowest)
-print(highest)
+KFRAW.close()
+print("sucessfully written pcm into .kftm")
+print("Lowest Value: " + str(lowest))
+print("Highest Value: " + str(highest))
